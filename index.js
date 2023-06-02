199b798f5b73dd01e2f2d114c724ab50234e721e
Nikhil <moranikhil951@gmail.com>
2023-06-02 20:48:22 +0000

    IPL RCBvsCSK

---
+++ index.js
@@ -0,0 +1,18 @@
+//   Write your code here.
+const element = (
+  <div className="container">
+    <h1 className="heading">Super Over League</h1>
+    <div class="min-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="img1"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="img2"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
